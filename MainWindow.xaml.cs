fb995c6129b66cb5b5f4b422fd293a707fae47fb
z1ra <apostolip@yandex.ru>
2024-12-17 14:48:07 +0300

    feat: Тестовый вариант

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,25 +1,41 @@
-﻿
-using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Attenuator
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // Обработчик кнопки "Рассчитать"
+        public void Test(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Считывание и преобразование входных данных
+                double Z = double.Parse(input_z.Text);
+                double R = double.Parse(input_r.Text);
+                double R1 = double.Parse(input_r1.Text);
+
+                double ratio = R / Z;
+                double A = (R - Z) / (R + Z);
+                double L = -20 * Math.Log10(Math.Abs(A));
+
+
+                // Вывод результатов
+                output.Text = $"Вычисленное значение L: {L:F3}";
+            }
+            catch (FormatException)
+            {
+                output.Text = "Ошибка: Введите корректные числа!";
+            }
+            catch (Exception ex)
+            {
+                output.Text = $"Ошибка: {ex.Message}";
+            }
         }
     }
 }
